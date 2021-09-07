36b6a31b77c5613a7d5ada0e1ae56ace98f3a74f
Yauheni Skibinski <stikler51@gmail.com>
2021-09-07 17:25:16 +0300

    add basic pages for routing

---
+++ src/pages/signIn.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import SignInForm from '../components/signInForm/signInForm';
+
+const SignIn = () => (
+  <>
+    <h1>Sign in page</h1>
+    <SignInForm />
+  </>
+);
+
+export default SignIn;
