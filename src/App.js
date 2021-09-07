baf794d9452e81c276c1bd1cd972d57ebfce7a31
Yauheni Skibinski <stikler51@gmail.com>
2021-09-07 17:25:44 +0300

    add router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import Header from './components/layout/header/header';
+import Home from './pages/home';
+import SignIn from './pages/signIn';
+import Register from './pages/register';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <div className="container">
+          <Switch>
+            <Route path="/signin">
+              <SignIn />
+            </Route>
+            <Route path="/register">
+              <Register />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
