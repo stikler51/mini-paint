0cb8c5515986de713aa8e7adc1448bf311ca2f35
Yauheni Skibinski <stikler51@gmail.com>
2021-09-08 12:40:28 +0300

    Rename form component for authorization

---
+++ src/components/authorizationForm/authorizationForm.js
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+// import { useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
+import {
+  Link,
+  useLocation
+} from 'react-router-dom';
+import styles from './authorizationForm.module.scss';
+
+const AuthorizationForm = ({ cb, action }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { pathname } = useLocation();
+
+  return (
+    <div className={styles.formWrapper}>
+      <div className={styles.authorizationForm}>
+        <div className="mb-2">
+          <input
+            type="email"
+            placeholder="E-mail"
+            className="form-control"
+            value={email}
+            onInput={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div className="mb-2">
+          <input
+            type="password"
+            placeholder="Password"
+            className="form-control"
+            value={password}
+            onInput={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <input
+          type="submit"
+          className="btn btn-primary"
+          onClick={() => cb(email, password)}
+          value={action}
+        />
+        {
+          pathname === '/signin' ? (
+            <span>
+              Don&apos;t have account?
+              <Link to="/register">Create one</Link>
+            </span>
+          ) : ''
+        }
+
+      </div>
+
+      {/* <div>
+        <input type="submit" onClick={() => registerUser(email, password)} value="Register" />
+
+        <button type="button" onClick={() => console.log(auth.currentUser)}>
+          Current user
+        </button>
+
+        <button type="button" onClick={signOutUser}>
+          Sign Out
+        </button>
+
+        <button type="button" onClick={() => console.log(user)}>
+          Show state
+        </button>
+      </div> */}
+    </div>
+  );
+};
+
+AuthorizationForm.propTypes = {
+  action: PropTypes.string,
+  cb: PropTypes.func
+};
+
+AuthorizationForm.defaultProps = {
+  action: 'Sign In',
+  cb: null
+};
+
+export default AuthorizationForm;
