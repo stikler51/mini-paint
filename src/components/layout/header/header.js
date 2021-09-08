218d8d49a455c9b523521564d40c5d60b7690f59
Yauheni Skibinski <stikler51@gmail.com>
2021-09-08 12:41:01 +0300

    add logout button

--- src/components/layout/header/header.js
+++ src/components/layout/header/header.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { signOutUser } from '../../../firebase/index';
 import styles from './header.module.scss';
 
-const Header = () => (
-  <header className={`${styles.header}`}>
-    <div className={`container ${styles.header_wrapper}`}>
-      <NavLink to="/">
-        <img className={styles.logo} src="/mini-paint-logo.png" alt="Logo" />
-      </NavLink>
-      <NavLink className="btn btn-success" to="signin">Sign In</NavLink>
-    </div>
-  </header>
-);
+const Header = () => {
+  const { loggedIn, user } = useSelector((state) => state.user.value);
+  return (
+    <header className={`${styles.header}`}>
+      <div className={`container ${styles.header_wrapper}`}>
+        <NavLink to="/">
+          <img className={styles.logo} src="/mini-paint-logo.png" alt="Logo" />
+        </NavLink>
+        {
+          loggedIn ? (
+            <div>
+              <span>
+                Hello,&nbsp;
+                {user.email}
+                !&nbsp;
+              </span>
+              <button className="btn btn-danger" type="button" onClick={signOutUser}>Log Out</button>
+            </div>
+          ) : <NavLink className="btn btn-success" to="signin">Sign In</NavLink>
+        }
+      </div>
+    </header>
+  );
+};
 
 export default Header;
