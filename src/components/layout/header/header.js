c8a7d50b07f9e442425db5a451802156a6731501
Yauheni Skibinski <stikler51@gmail.com>
2021-09-08 11:20:34 +0300

    add logo to header

--- src/components/layout/header/header.js
+++ src/components/layout/header/header.js
@@ -1,15 +1,14 @@
 import React from 'react';
-
 import { NavLink } from 'react-router-dom';
 import styles from './header.module.scss';
 
 const Header = () => (
   <header className={`${styles.header}`}>
     <div className={`container ${styles.header_wrapper}`}>
-      <h2>Mini-paint</h2>
-      <NavLink to="signin">Sign In</NavLink>
-      <NavLink to="register">Register</NavLink>
-      {/* <button type="button" className="btn btn-info">Sign in</button> */}
+      <NavLink to="/">
+        <img className={styles.logo} src="/mini-paint-logo.png" alt="Logo" />
+      </NavLink>
+      <NavLink className="btn btn-success" to="signin">Sign In</NavLink>
     </div>
   </header>
 );
