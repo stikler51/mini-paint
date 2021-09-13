b9a4a2ca76f6aa33ab2fd79f638d59ff93aedef7
Yauheni Skibinski <stikler51@gmail.com>
2021-09-13 17:17:59 +0300

    refactor firebase auth section

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -1,4 +1,3 @@
-import { initializeApp } from 'firebase/app';
 import {
   getAuth,
   signInWithEmailAndPassword,
@@ -6,20 +5,12 @@ import {
   signOut,
   onAuthStateChanged
 } from 'firebase/auth';
+import firebaseApp from './firebase';
 import store from '../store/store';
 import { login, logout, setError } from '../store/userSlice';
 import { startLoading, stopLoading } from '../store/loadingSlice';
 
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_API_ID
-};
-
-initializeApp(firebaseConfig);
+firebaseApp();
 
 export const auth = getAuth();
 
