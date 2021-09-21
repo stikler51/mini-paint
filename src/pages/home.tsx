2809c65026dc224d2f6db82af876418cb80eb66a
Yauheni Skibinski <stikler51@gmail.com>
2021-09-21 11:27:18 +0300

    add home banner

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,11 +3,19 @@ import { Link } from 'react-router-dom';
 
 const Home = () => (
   <>
-    <h1>Home page</h1>
-    <Link className="btn btn-success" to="/editor">Start drawing</Link>
-    <Link className="btn btn-success" to="/gallery">Art Gallery</Link>
+    <div className="homeBanner">
+      <h1 className="homeBanner__title">
+        The best web application (really not)
+        <br />
+        for drawing&nbsp;
+        <span className="easterEgg" />
+      </h1>
+      <div className="mainPageButtons">
+        <Link className="btn btn-success" to="/editor">Start drawing</Link>
+        <Link className="btn btn-success" to="/gallery">Art Gallery</Link>
+      </div>
+    </div>
   </>
-
 );
 
 export default Home;
